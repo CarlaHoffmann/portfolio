61a6ca11db3011b1aa0a1f441e09780795370afd
CarlaHoffmann <carlahoffmann@gmx.de>
2025-05-19 13:33:56 +0200

    added contactform

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContactformComponent } from './contactform/contactform.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ContactformComponent},
+    // { path:'game/:id', component: GameComponent}
+];
