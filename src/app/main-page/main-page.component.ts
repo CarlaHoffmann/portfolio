01374c1e4a609397205b5f42ca0a62811c12d729
CarlaHoffmann <carlahoffmann@gmx.de>
2025-05-20 18:20:05 +0200

    added page component

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { PageComponentComponent } from '../page-component/page-component.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, PageComponentComponent, FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
