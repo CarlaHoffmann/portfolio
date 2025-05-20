38811be5435a451b0b08bfcce39d127b4b0b42be
CarlaHoffmann <carlahoffmann@gmx.de>
2025-05-20 17:48:16 +0200

    added styling

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MenuComponent } from '../menu/menu.component';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [MenuComponent, HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
