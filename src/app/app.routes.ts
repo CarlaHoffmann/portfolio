025e21c9c347f4545ea9f17e1501406dc90eafe9
CarlaHoffmann <carlahoffmann@gmx.de>
2025-05-21 11:45:34 +0200

    added component and style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { HeaderComponent } from './header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 export const routes: Routes = [
-    { path: '/', component: MainPageComponent},
+    { path: '', component: MainPageComponent},
     // { path: '', component: MenuComponent},
     // { path: '', component: HeaderComponent},
     // { path: '', component: ContactformComponent},
-    { path:'/contact', component: ContactformComponent}
+    { path:'contact', component: ContactformComponent}
 ];
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { PageComponentComponent } from '../page-component/page-component.component';
 import { SkillsComponent } from '../skills/skills.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, PageComponentComponent, SkillsComponent, FooterComponent],
+  imports: [HeaderComponent, PageComponentComponent, SkillsComponent, ProjectsComponent, FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
