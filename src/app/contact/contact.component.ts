c4c4b0f5cd3583009cfec274dea072101e00e096
CarlaHoffmann <carlahoffmann@gmx.de>
2025-05-26 12:35:17 +0200

    added contact style

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { SlideBtnComponent } from '../slide-btn/slide-btn.component';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [FormsModule, SlideBtnComponent],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+  http = inject(HttpClient);
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  // onSubmit(ngForm: NgForm) {
+  //   if(ngForm.valid && ngForm.submitted) {
+  //     console.log(this.contactData);
+  //   }
+  // }
+  mailTest = false;
+
+  post = {
+    endPoint: 'https://carla-hoffmann.net/sendMail.php', // 'https://deineDomain.de/sendMail.php'
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => { 
+            
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.log('Test mail');
+      ngForm.resetForm();
+    }
+  }
+}
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -4,11 +4,14 @@ import { FooterComponent } from '../footer/footer.component';
 import { PageComponentComponent } from '../page-component/page-component.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { ProjectsComponent } from '../projects/projects.component';
+import { ContactComponent } from '../contact/contact.component';
+import { ReferenceComponent } from '../reference/reference.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, PageComponentComponent, SkillsComponent, ProjectsComponent, FooterComponent],
+  imports: [HeaderComponent, PageComponentComponent, SkillsComponent, 
+    ProjectsComponent, ReferenceComponent, ContactComponent, FooterComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
