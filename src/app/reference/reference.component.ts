e7dc0daf59c9f0568d0ed3a31bd769ecacac319d
CarlaHoffmann <carlahoffmann@gmx.de>
2025-05-28 13:12:30 +0200

    added projects slide control

--- src/app/reference/reference.component.ts
+++ src/app/reference/reference.component.ts
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './reference.component.scss'
 })
 export class ReferenceComponent {
+  slides = [
+    { text: "Carla is a fantastic team player!", author: "Anna" },
+    { text: "Her code quality is outstanding.", author: "Ben" },
+    { text: "Always positive and helpful.", author: "Dario" },
+    { text: "Brings great energy to every project.", author: "Eva" }
+  ];
 
+  currentIndex = 0;
+
+  get prevIndex() {
+    return (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  get nextIndex() {
+    return (this.currentIndex + 1) % this.slides.length;
+  }
+
+  goPrev() {
+    this.currentIndex = this.prevIndex;
+  }
+
+  goNext() {
+    this.currentIndex = this.nextIndex;
+  }
 }
