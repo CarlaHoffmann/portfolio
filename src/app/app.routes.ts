5236af9a14a1de2dc9ce1ee16152d988516aa9a0
CarlaHoffmann <carlahoffmann@gmx.de>
2025-06-07 15:41:28 +0200

    added translation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ContactformComponent } from './contactform/contactform.component';
+// import { ContactformComponent } from './contactform/contactform.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { LogoInitialsComponent } from '../logo-initials/logo-initials.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [LogoComponent, LogoInitialsComponent],
+  imports: [LogoComponent, LogoInitialsComponent, RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
