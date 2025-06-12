bd1ece04ddd49df604762e29823050c8cf5ef299
CarlaHoffmann <carlahoffmann@gmx.de>
2025-06-12 15:32:17 +0200

    added

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LogoComponent } from '../logo/logo.component';
 import { LogoInitialsComponent } from '../logo-initials/logo-initials.component';
 import { RouterModule } from '@angular/router';
+import { TranslationService } from '../services/translation.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -10,6 +12,35 @@ import { RouterModule } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
+  translations = {
+    en: {
+      legal: 'Legal notice'
+    },
+    de: {
+      legal: 'Impressum'
+    }
+  };
 
+  lang: 'en' | 'de' = 'en';
+  t: any = this.translations.en;
+
+  private langSub!: Subscription;
+
+  constructor(private translation: TranslationService) {}
+
+  ngOnInit() {
+    // Sprachwechsel abonnieren
+    this.langSub = this.translation.lang$.subscribe(lang => {
+      this.lang = lang;
+      this.t = this.translations[lang];
+    });
+    // Initial setzen
+    this.lang = this.translation.currentLang;
+    this.t = this.translations[this.lang];
+  }
+
+  ngOnDestroy() {
+    this.langSub?.unsubscribe();
+  }
 }
